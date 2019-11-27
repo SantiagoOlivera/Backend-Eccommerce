c07fa18dd347c890c5473169ebfa9215dfef0de2
SantiagoOlivera <olivera.santiagoariel@gmail.com>
2019-11-27 12:23:09 -0300

    Product design form

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,24 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
 import { ProductsService } from '../services/products.service';
 import { Router } from '@angular/router';
+import { MatFileUploadModule } from 'angular-material-fileupload';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css'],
-  providers: [ ProductsService ]
+  providers: [ ProductsService ],
+  
 
 })
+/* @NgModule({
+  imports: [MatExpansionModule]
+}) */
 export class ProductsComponent implements OnInit {
   
   productForm;
   data = [];
-  newProducts: String = "";
+  newProducts: Array<Number> = [];/* Array<MatExpansionModule> = []; */
+
 
   //el metodo constructor se ejecuta cuando se inicia la carga 
   //del componente
   constructor( public productsService: ProductsService, private router: Router) { 
-      this.getProducts();
+          this.getProducts();
   }
 
   getProducts(){
@@ -32,7 +38,9 @@ export class ProductsComponent implements OnInit {
   }
 
   addProduct(){
+    //console.log(this.newProducts);
+    this.newProducts.push(new Number());
     console.log(this.newProducts);
-    this.newProducts = this.newProducts + '<mat-accordion><mat-expansion-panel><mat-expansion-panel-header><mat-panel-title>{{ products.name }}</mat-panel-title><mat-panel-description>Type your name and age</mat-panel-description></mat-expansion-panel-header><mat-form-field><input matInput placeholder="First name"></mat-form-field><mat-form-field><input matInput placeholder="Age"></mat-form-field></mat-expansion-panel></mat-accordion>';
+    /* this.newProducts = this.newProducts + '<mat-accordion><mat-expansion-panel><mat-expansion-panel-header><mat-panel-title>{{ products.name }}</mat-panel-title><mat-panel-description>Type your name and age</mat-panel-description></mat-expansion-panel-header><mat-form-field><input matInput placeholder="First name"></mat-form-field><mat-form-field><input matInput placeholder="Age"></mat-form-field></mat-expansion-panel></mat-accordion>'; */
   }
 }
